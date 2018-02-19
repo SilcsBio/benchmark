95c3707444e03c24aacdf95c12884e1f7cd81941
cfncluster user <ubuntu@ip-172-31-43-55.us-west-2.compute.internal>
2018-02-19 17:53:36 +0000

    moved to medium folder

--- medium/run.cmd
+++ medium/run.cmd
@@ -1,7 +1,16 @@
+#$ -S /bin/bash
+#$ -cwd
+#$ -V
+#$ -o benchmark.out
+#$ -j y
+#$ -N benchmark
+#$ -l h_data=1000M,h_rt=240:00:00
+#$ -pe mpi 8
+#$ -R y
 
 # please edit these to match your setting
-nproc=<nproc>                                 # number of cpu cores to use
-GMXDIR=/usr/local/bin                   # path to gmx
+nproc=8                                 # number of cpu cores to use
+GMXDIR=/shared/apps/gromacs/2018.1      # path to gmx
 mdrun="${GMXDIR}/gmx mdrun -nt $nproc"  # gmx mdrun command
 
 # do not edit below
